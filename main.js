1ce92d1def801083d4c35e19303279819f5b596f
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-10-14 13:31:55 -0400

    piece movement

--- main.js
+++ main.js
@@ -40,14 +40,56 @@ const map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+// create the piece
+const piece = {
+  position: {x: 6, y: 2},
+
+  shape: [
+    [1, 1],
+    [1, 1]
+  ]
+
+}
+
 function upDate() {
 
   Draw()
+
+  window.requestAnimationFrame(upDate)
 }
 
 function Draw() {
+  // paitn the canvas
   context.fillStyle = '#000'
   context.fillRect(0, 0, canvas.width, canvas.height)
+
+  // paint the piece and pieces solids
+  map.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1){
+        context.fillStyle = 'yellow'
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
+  
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1) {
+        context.fillStyle = 'red'
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+      }
+    })
+  })
 }
+
+
+// pieces muvement
+document.addEventListener('keydown', event => {
+  if(event.key === 'ArrowLeft') piece.position.x--
+  if(event.key === 'ArrowRight') piece.position.x++
+  if(event.key === 'ArrowDown') piece.position.y++
+})
+
 
 upDate()
